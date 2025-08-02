e935e555d2e11fe646a4c418c79691731ac993e5
nikiTa011777 <nikitaghimire2025@gmail.com>
2025-08-02 21:16:12 +0545

    chore: add environment constants and test data configuration

---
+++ config/testData.ts
@@ -0,0 +1,13 @@
+export const users = {
+    domain:'810-sbx',
+    username: 'demouser2',
+    password: 'D3m0Us3r!#',
+  
+};
+
+export const formData = {
+    name: "hari shyam",
+    email : "harishyam@gmail.com",
+   
+};
+
